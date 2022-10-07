5520a0cabe48fd9ce20eaceb3fd7caf01b8da8fb
Kostas <kostasspanolios@gmail.com>
2022-10-07 07:27:27 +0100

    JS-like string formatting, accepting user input

--- Formatting/Program.cs
+++ Formatting/Program.cs
@@ -9,3 +9,11 @@ string formatted = string.Format(
  arg0: numberOfApples,
  arg1: pricePerApple * numberOfApples);
 //WriteToFile(formatted); // writes the string into a file
+// string us = dec.ToString("C", new CultureInfo("en-US"));
+Console.WriteLine($"{numberOfApples} apples costs {pricePerApple * numberOfApples:C}");
+Console.Write("Type your first name and press ENTER: ");
+string firstName = Console.ReadLine();
+Console.Write("Type your age and press ENTER: ");
+string age = Console.ReadLine();
+Console.WriteLine(
+ $"Hello {firstName}, you look good for {age}.");
