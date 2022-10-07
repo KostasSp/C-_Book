5f6ebbb7d625a7dc43a20b657c590a975cf88845
Kostas <kostasspanolios@gmail.com>
2022-10-07 14:08:01 +0100

    unary operators

---
+++ Chapter 03/Program.cs
@@ -0,0 +1,9 @@
+﻿using static System.Console;
+
+int a = 3;
+int b = a++;
+WriteLine($"a is {a}, b is {b}");
+
+int c = 3;
+int d = ++c; // increment c before assigning it
+WriteLine($"c is {c}, d is {d}");
